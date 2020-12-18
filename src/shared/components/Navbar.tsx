8bdb6823f4bf681aaffcb129ec34e1617825e366
Cristian Alarcón <calarconpieriz@gmail.com>
2020-12-18 15:56:59 -0300

    Remove unused code

--- src/shared/components/Navbar.tsx
+++ src/shared/components/Navbar.tsx
@@ -2,14 +2,12 @@ import React from 'react';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
-import InputBase from '@material-ui/core/InputBase';
 import {
   createStyles,
   fade,
   Theme,
   makeStyles,
 } from '@material-ui/core/styles';
-import SearchIcon from '@material-ui/icons/Search';
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
