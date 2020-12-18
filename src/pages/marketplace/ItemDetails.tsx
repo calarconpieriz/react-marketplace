2614d45f05a30135094fb0835e4c514e3a7bcc8b
Cristian Alarcón <calarconpieriz@gmail.com>
2020-12-18 13:36:48 -0300

    Details added

--- src/pages/marketplace/ItemDetails.tsx
+++ src/pages/marketplace/ItemDetails.tsx
@@ -1,63 +1,86 @@
 import React, { useEffect } from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Card from '@material-ui/core/Card';
-import CardActionArea from '@material-ui/core/CardActionArea';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia';
+import { makeStyles, Theme, createStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 import { connect } from 'react-redux';
 import { fetchItemDetails } from './store/item.actions';
 import { Link } from 'react-router-dom';
-import { Avatar, Grid, Paper } from '@material-ui/core';
+import { Grid, Paper } from '@material-ui/core';
+import CheckIcon from '@material-ui/icons/Check';
+import PersonIcon from '@material-ui/icons/Person';
+import LoopIcon from '@material-ui/icons/Loop';
+import TextureIcon from '@material-ui/icons/Texture';
+import LineWeightIcon from '@material-ui/icons/LineWeight';
 
-const useStyles = makeStyles({
-  root: {},
-});
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      marginLeft: '3%',
+    },
+    title: {
+      textTransform: 'uppercase',
+    },
+    paper: {
+      padding: theme.spacing(2),
+      textAlign: 'left',
+      color: theme.palette.text.secondary,
+    },
+  })
+);
 
 const Item = ({ name, details, fetchItemDetails }: any) => {
   const classes = useStyles();
+
+  useEffect(() => {
+    fetchItemDetails();
+  }, [fetchItemDetails]);
+
   return (
-    <Grid container spacing={3} onLoad={() => fetchItemDetails()}>
-      <Grid item xs={12}>
-        <h1>{name}</h1>
-        <Paper variant="outlined">
-          <div>
+    <div className={classes.root}>
+      <h1 className={classes.title}>{name}</h1>
+      <Grid container spacing={1}>
+        <Grid item xs={5}>
+          <Paper className={classes.paper} variant="outlined">
             <img alt="front" src={details.sprites.front_default} />
-
             <img alt="back" src={details.sprites.back_default} />
-
             <img alt="front shiny" src={details.sprites.front_shiny} />
-          </div>
-        </Paper>
-      </Grid>
-
-      <Card className={classes.root}>
-        <CardActionArea>
-          <CardMedia
-            component="img"
-            alt="Contemplative Reptile"
-            height="250"
-            image={details.sprites.back_default}
-            title="Contemplative Reptile"
-          />
-          <CardContent>
-            <Typography gutterBottom variant="h5" component="h2">
-              {name}
-              {details.weight}
+            <Typography>
+              <PersonIcon />
+              <strong>Nombre:</strong> {name}
             </Typography>
-          </CardContent>
-        </CardActionArea>
-        <CardActions>
+            <Typography>
+              <LineWeightIcon />
+              <strong>Peso:</strong> {details.weight}
+            </Typography>
+            {details.abilities.map((item: any, index: number) => (
+              <Typography>
+                <CheckIcon />
+                <strong>Habilidad {index + 1}:</strong> {item.ability.name}
+              </Typography>
+            ))}
+            {details.forms.map((item: any, index: number) => (
+              <Typography>
+                <LoopIcon />
+                <strong>Forma {index + 1}:</strong> {item.name}
+              </Typography>
+            ))}
+            {details.types.map((item: any, index: number) => (
+              <Typography>
+                <TextureIcon />
+                <strong>Tipo {index + 1}:</strong> {item.type.name}
+              </Typography>
+            ))}
+          </Paper>
+        </Grid>
+        <Grid item xs={12}>
           <Link key={name} to="/">
-            <Button size="small" color="primary" href="/">
+            <Button size="small" color="primary">
               Volver
             </Button>
           </Link>
-        </CardActions>
-      </Card>
-    </Grid>
+        </Grid>
+      </Grid>
+    </div>
   );
 };
 
